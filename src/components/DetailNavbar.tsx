01c6f4721c09b8ec70d257f4842147ce2d148883
greg-hugo <hugo.gregory5@gmail.com>
2023-10-29 21:40:57 +0700

    refactor: move state and callback

--- src/components/DetailNavbar.tsx
+++ src/components/DetailNavbar.tsx
@@ -3,31 +3,26 @@
 import { useState, useCallback } from "react"
 import NavbarButton from "./NavbarButton"
 
+interface DetailNavbarProps {
+    menuItems: string[];
+    selectedItem: string;
+    setSelectedItem: React.Dispatch<React.SetStateAction<string>>;
+}
 
-
-const menuItems = [
-    "About",
-    "Base Stats",
-    "Evolution",
-    "Moves"
-];
-
-const DetailNavbar = () => {
-
-    const [selected, setSelected] = useState<string>(menuItems[0]);
+const DetailNavbar = ({ menuItems, selectedItem, setSelectedItem }: DetailNavbarProps) => {
 
     const handleClick = useCallback((item: string) => {
-        setSelected(item);
-    }, []);
+        setSelectedItem(item);
+    }, [setSelectedItem]);
 
     return (
-        <div className="bg-inherit flex justify-evenly w-full">
+        <div className="bg-inherit flex justify-between w-full mb-12">
             {
                 menuItems.map((item: string) => (
                     <NavbarButton 
                         text={item} 
                         onClick={() => handleClick(item)} 
-                        selected={selected === item} 
+                        selected={selectedItem === item} 
                         key={item}
                     />
                 ))
