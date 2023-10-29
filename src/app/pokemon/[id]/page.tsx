54f0a0289b95ef161b801c8bf4615dcda4a6dbf2
greg-hugo <hugo.gregory5@gmail.com>
2023-10-29 21:43:49 +0700

    feat: add pokemon infos

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -6,9 +6,22 @@ import { getTypeColor } from "@/utils/typecolor";
 import PokemonTypePill from "@/components/PokemonTypePill";
 import DetailNavbar from "@/components/DetailNavbar";
 import AboutInfo from "@/components/AboutInfo";
+import StatsInfo from "@/components/StatsInfo";
+import EvolutionChain from "@/components/EvolutionChain";
+import MoveInfo from "@/components/MoveInfo";
+import Link from "next/link";
+
+
+const menuItems = [
+    "About",
+    "Base Stats",
+    "Evolution",
+    "Moves"
+];
 
 export default function Page({ params }: { params: { id: number } }) {
     const [pokemon, setPokemon] = useState<PokemonDetail>();
+    const [selectedItem, setSelectedItem] = useState<string>(menuItems[0]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -27,14 +40,21 @@ export default function Page({ params }: { params: { id: number } }) {
     }, [pokemon]);
 
     return (
-        <div className="h-screen " style={{ backgroundColor: `${getBackground()}` }}>
-            <div className="flex justify-between p-8">
+        <div className="h-screen flex flex-col" style={{ backgroundColor: `${getBackground()}` }}>
+            <Link href={'/'} className="pt-3 opacity-25 hover:opacity-100 w-16">
+                <span className="px-8">Home</span>
+            </Link>
+            <div className="flex justify-between pl-8 pt-5">
                 <div>
                     <h1 className="mb-2 capitalize"> {pokemon?.name} </h1>
-                    <div className="flex space-x-2">
+                    <div className="flex">
                         {
                             pokemon?.types.map((type: SimplePokemonType, index: number) => {
-                                return <PokemonTypePill type={type.type.name} key={index}/>
+                                return (
+                                    <div key={index} className="w-28">
+                                        <PokemonTypePill type={type.type.name} />
+                                    </div>
+                                )
                             })
                         }
                     </div>
@@ -53,9 +73,33 @@ export default function Page({ params }: { params: { id: number } }) {
                     <img src={pokemon?.sprites.front_default} className='h-full w-full' alt='Pokemon'/>
                 </div>
             </div>
-            <div className="h-96 bg-white rounded-tl-3xl rounded-tr-3xl pt-24 px-8">
-                <DetailNavbar />
-                <AboutInfo />
+            <div className="flex-grow bg-white rounded-tl-3xl rounded-tr-3xl pt-24 px-20">
+                <DetailNavbar 
+                    menuItems={menuItems}
+                    selectedItem={selectedItem}
+                    setSelectedItem={setSelectedItem}
+                />  
+                {
+                    selectedItem === "About" &&
+                    <AboutInfo 
+                        species={pokemon?.species.name} 
+                        height={pokemon?.height}
+                        weight={pokemon?.weight}
+                        abilities={pokemon?.abilities.map((ability) => ability.ability.name)}
+                    />
+                }
+                {
+                    selectedItem === "Base Stats" && pokemon &&
+                    <StatsInfo stats={pokemon?.stats}/>
+                }
+                {
+                    selectedItem === "Evolution" && pokemon?.evolution_chain &&
+                    <EvolutionChain pokemons={pokemon.evolution_chain}/>
+                }
+                {
+                    selectedItem === "Moves" && pokemon?.moves &&
+                    <MoveInfo moves={pokemon.moves} />
+                }
             </div>
         </div>
     )
