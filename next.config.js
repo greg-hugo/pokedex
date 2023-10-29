46d38c7f62b38380997eff4ae6c4f4cebd63c6e9
greg-hugo <hugo.gregory5@gmail.com>
2023-10-29 23:51:34 +0700

    del: remove svgr

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config, options) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: [
-                {
-                    loader: '@svgr/webpack',
-                    options: {icon: true}
-                }
-            ]
-        });
-
-    return config
-  },
+    
 }
 
 module.exports = nextConfig;
